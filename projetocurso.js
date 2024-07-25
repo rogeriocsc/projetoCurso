99ec320c0158686f90ff8f7dc17b61af333d9e45
Rogerio Curtio <rogeriocurtiogithub@gmail.com>
2024-07-24 23:32:01 -0300

    Atualização html, css, javascript

--- projetocurso.js
+++ projetocurso.js
@@ -1,14 +1,27 @@
 const cursosTodos = [...document.getElementsByClassName("curso")]
-const cursosC1 = [...document.getElementsByClassName("c1")]
-const cursosC2 = [...document.getElementsByClassName("c2")]
-console.log(cursosTodos)
-console.log(cursosC1)
-console.log(cursosC2)
+const cursosa1 = [...document.getElementsByClassName("a1")]
+const cursosa2 = [...document.getElementsByClassName("a2")]
+const cursosa3 = [...document.getElementsByClassName("a3")]
+const cursosa4 = [...document.getElementsByClassName("a4")]
+const cursos = [...document.querySelectorAll(".curso")]
+const cursoEspecial = document.getElementsByClassName("curso")[6]
 
-cursosC1.map((el)=>{
-    el.classList.add("destaque")
+// console.log(cursosTodos)
+// console.log(cursosa1)
+// console.log(cursosa2)
+// console.log(cursosa3)
+// console.log(cursosa4)
+// console.log(cursos)
+// console.log(cursoEspecial)
+
+// cursosa2.map((el)=> {
+//     el.classList.add("destaque")
+// })
+
+cursos.map((el)=>{
+    el.addEventListener('click',(evt)=>{
+        const el = evt.target
+        el.classList.add("destaque")
+        console.log(el.innerHTML + " Foi Clicado!")
+    })
 })
-
-// Add classe destaque em cursosC1
-// Método getElementsByClassName permite obter os elementos
-// do DOM que utiliza uma classe específica.
